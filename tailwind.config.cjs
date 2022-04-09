653454793d40e72ab346791c97fe239a58e87b32
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-04-09 16:25:04 +0100

    css variables working for prose theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      text: "var(--color-text)",
-      background: "var(--color-background)",
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": theme("colors.text"),
+            "--tw-prose-invert-body": theme("colors.text"),
+          },
+        },
+      }),
+      colors: ({ colors }) => ({
+        text: "var(--color-text)",
+        background: "var(--color-background)",
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
