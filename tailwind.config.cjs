8d29707977b6502f0aca48af2b3ce83ef6eb049c
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-04-09 14:35:24 +0100

    style changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    colors: {
+      text: "var(--color-text)",
+      background: "var(--color-background)",
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
