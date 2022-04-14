b59bfcce132872314287cf8b05d6a51a63388d63
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-04-14 23:47:37 +0100

    add sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
 import turbolinks from "@astrojs/turbolinks";
-
 import astroImagePlugin from "astro-imagetools/plugin";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     svelte(),
-    tailwind({ config: { applyAstroPreset: false } }),
+    tailwind({
+      config: {
+        applyAstroPreset: false,
+      },
+    }),
     turbolinks(),
+    sitemap(),
   ],
-  vite: { plugins: [astroImagePlugin] },
+  vite: {
+    plugins: [astroImagePlugin],
+  },
 });
